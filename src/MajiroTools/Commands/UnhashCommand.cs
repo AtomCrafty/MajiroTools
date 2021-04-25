4ba1e16ffa81f38b8295dc18c161cd899445521b
AtomCrafty <atomcrafty@frucost.net>
2021-04-25 22:29:31 +0200

    Unhash command

---
+++ src/MajiroTools/Commands/UnhashCommand.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using Majiro;
+using Majiro.Script;
+using VToolBase.Cli;
+
+namespace MajiroTools.Commands {
+	public class UnhashCommand : Command {
+		public UnhashCommand(CommandParameters parameters) : base(parameters) { }
+
+		public override string Name => "unhash";
+
+		public override string[] Description => new[] {
+			"Brute forces possible strings that could result in a specific hash"
+		};
+
+		public override (string syntax, string description)[] Usage => new[] {
+			("\abhash", "Searches for names matching the given hash"),
+			("\abhash prefix", "Searches for a string to replace the single \ab*\a- within the pattern such that the result matches the given hash"),
+			("\abhash prefix charset", "Additionally specify the set of valid characters")
+		};
+
+		public override (char shorthand, string name, string fallback, string description)[] Flags => new[] {
+			('n', "max-length", "10", "The maximum number of characters, excluding prefix and suffix"),
+			('m', "min-length", "0", "The minimum number of characters, excluding prefix and suffix"),
+			('p', "pattern", "\"*\"", "The name search pattern. Has to contain exactly one \ab*\a-, which represents the unknown part."),
+			('c', "charset", "\"a-z_0-9\"", "The set of allowed characters. Allows simple range specifications."),
+			('a', "alpha", null, "Overwrites the charset with \ab_\a- and the english alphabet."),
+
+			('s', "syscall", "false", $"Shortcut for \ac--group={Data.SyscallGroup}"),
+			('l', "local", "false", "Prepend \ab_\a- to the prefix and append \ab@\a- to the suffix"),
+			('g', "group", null, "Prepend \ab$\a- to the prefix and append \ab@<group>\a- to the suffix"),
+
+			('q', "quiet", "false", "Disable user-friendly output"),
+			('w', "wait", "false", "Whether to wait after completing the command")
+		};
+
+		public override bool Execute() {
+
+			if(Arguments.Length == 0)
+				throw new ArgumentException("Not enough arguments");
+
+			if(!uint.TryParse(Arguments[0], NumberStyles.HexNumber, CultureInfo.InvariantCulture, out uint targetHash))
+				throw new ArgumentException("Invalid hash. Please specify it as a 32 bit hex number");
+
+			bool isSyscall = Data.SyscallHashes.Contains(targetHash);
+			int minLength = Parameters.GetInt("min-length", 'm', 0);
+			int maxLength = Parameters.GetInt("max-length", 'n', 10);
+
+			string pattern = Parameters.GetString("pattern", 'p', Arguments.Length > 1 ? Arguments[1] : "*");
+			string charset = Parameters.GetString("charset", 'c', Arguments.Length > 2 ? Arguments[2] : CrcUnhasher.DefaultCharset);
+
+			string group = Parameters.GetString("group", 'g', null);
+			bool isLocal = Parameters.GetBool("local", false);
+
+			if(Parameters.GetBool("alpha", 'a', false)) {
+				charset = "_abcdefghijklmnopqrstuvwxyz"; // ordered lexicographically
+				charset = "_toiswcbphfmderlnagukvyjqxz"; // ordered by how likely a given character is to be the first in a word (text)
+				charset = "_scpdbramtfiehgluwonvjkqyzx"; // ordered by how likely a given character is to be the first in a word (dictionary)
+				charset = "_etaoinshrdlcumwfgypbvkjxqz"; // ordered by letter frequency (text)
+				charset = "_esuarntolcdugpmhbqfvkwzxjq"; // ordered by letter frequency (dictionary)
+			}
+
+			if(Parameters.GetBool("syscall", 's', false))
+				group = Data.SyscallGroup;
+
+			if(group != null && isLocal)
+				throw new ArgumentException("Can't have a local with a non-null group");
+
+			if(isLocal) {
+				pattern = '_' + pattern + '@';
+			}
+
+			if(group != null) {
+				pattern = '$' + pattern + '@' + group;
+			}
+
+			var parts = pattern.Split('*');
+			if(parts.Length != 2)
+				throw new ArgumentException($"\acPattern must contain exactly one asterisk [\ab{pattern}\ac]");
+
+			string prefix = parts[0];
+			string suffix = parts[1];
+
+			Log($"Target:  \ab{targetHash:x8}", true);
+			Log($"Length:  \ab{minLength}\a- to \ab{maxLength}", true);
+			Log($"Pattern: \ab{prefix}\ac*\ab{suffix}", true);
+			Log($"Charset: \ab{charset}", true);
+			Log("", true);
+
+			var unhasher = new CrcUnhasher {
+				Target = targetHash,
+				Prefix = prefix,
+				Postfix = suffix,
+				Charset = charset,
+			};
+
+			ulong totalChecks = 0;
+			uint totalMatches = 0;
+			uint checksSinceLastUpdate = 0;
+
+			var sw = Stopwatch.StartNew();
+			const int updateInterval = 100;
+
+			for(int length = minLength; length <= maxLength; length++) {
+				Log($"Searching for matches of length \ab{length}\a-");
+				unhasher.Length = length;
+
+				uint lengthMatches = 0;
+
+				foreach(string match in unhasher) {
+					checksSinceLastUpdate++;
+					if(sw.ElapsedMilliseconds > updateInterval) {
+						totalChecks += checksSinceLastUpdate;
+						Output.WriteColored($"Total checks: \ab{totalChecks}\a- Hashes per ms: \ab{(float)checksSinceLastUpdate / sw.ElapsedMilliseconds}\r");
+						sw.Restart();
+						checksSinceLastUpdate = 0;
+					}
+					if(match == null) continue;
+					lengthMatches++;
+					Output.ClearLine();
+					Output.Write(targetHash.ToString("x8"), isSyscall ? ConsoleColor.Red : ConsoleColor.Blue);
+					Output.WriteLine(' ' + match);
+				}
+
+				Output.ClearLine();
+				if(lengthMatches != 0) {
+					Log($"Found \ab{lengthMatches}\a- match{(lengthMatches != 1 ? "es" : "")} of length \ab{length}\a-");
+					totalMatches += lengthMatches;
+				}
+			}
+
+			return true;
+		}
+	}
+}
--- src/MajiroTools/Program.cs
+++ src/MajiroTools/Program.cs
@@ -1,86 +1,28 @@
-﻿using System;
-using System.IO;
-using Majiro.Script;
-using Majiro.Script.Analysis.ControlFlow;
-using Majiro.Script.Analysis.StackTransition;
-using Majiro.Util;
+﻿using System.Globalization;
+using System.Linq;
+using MajiroTools.Commands;
 using VToolBase.Cli;
 using VToolBase.Cli.Commands;
-using VToolBase.Cli.Util;
 
 namespace MajiroTools {
 
 	public class Program {
 		public static void Main(string[] args) {
+			CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+			HelpCommand.AppName = "MajiroTool Cli";
+			HelpCommand.LibName = "MajiroLib";
+
+			CommandManager.Register("hash", parameters => new HashCommand(parameters));
+			CommandManager.Register("unhash", parameters => new UnhashCommand(parameters));
+			CommandManager.Register("disassemble", parameters => new DisassembleCommand(parameters));
+
 			if(!CommandManager.TryRun(args)) {
-				new HelpCommand(CommandParameters.Empty).Execute();
+				if(args.Length == 0)
+					new HelpCommand(CommandParameters.Empty).Execute();
+				else
+					new AutoCommand(CommandParameters.ParseArguments(args.Prepend("auto"))).Execute();
 			}
 			Output.Flush();
-		}
-
-		static void PrintHash(string name) {
-			Console.WriteLine($"{Crc.Hash32(name):x8} {name}");
-		}
-
-		static void Main2(string[] args) {
-			var names = new[] {
-				"X_CONTROL",
-				"user_voice",
-				"user_voice@GLOBAL",
-			};
-
-			foreach(string name in names) {
-				//PrintHash(name);
-			}
-
-			/*
-			foreach(var opcode in Opcode.List) {
-				Console.Write($"{opcode.Value:X3} {opcode.Mnemonic.PadRight(13)}");
-				Console.WriteLine(opcode.Aliases.Any() ? $" (aliases: {string.Join(", ", opcode.Aliases)})" : "");
-			}
-			//*/
-
-			//*
-			const string inPath = "start.mjo";//@"D:\Games\Private\[Jast] Closed Game [v16700]\scenario\0010101.mjo";
-			using var reader = new BinaryReader(File.OpenRead(inPath));
-			var script = Disassembler.DisassembleScript(reader);
-
-			ControlFlowPass.Analyze(script);
-			StackTransitionPass.Analyze(script);
-
-			//Disassembler.PrintScript(script, IColoredWriter.Console);
-			using(var fw = new StreamWriter("script1.mjil"))
-				Disassembler.PrintScript(script, new StreamColorWriter(fw));
-
-			using var ms = new MemoryStream();
-			using(var sw = new StreamWriter(ms, null, -1, true)) {
-				Disassembler.PrintScript(script, new StreamColorWriter(sw));
-			}
-
-			ms.Position = 0;
-			using var sr = new StreamReader(ms);
-			var script2 = Assembler.Parse(sr);
-
-			ControlFlowPass.Analyze(script2);
-			StackTransitionPass.Analyze(script2);
-
-			using(var fw = new StreamWriter("script2.mjil"))
-				Disassembler.PrintScript(script2, new StreamColorWriter(fw));
-
-			using(var fw = File.Open("exported.mjo", FileMode.Create)) {
-				Assembler.AssembleScript(script2, new BinaryWriter(fw), true, false);
-				fw.Flush(true);
-				fw.Close();
-			}
-
-			using var reader2 = new BinaryReader(File.OpenRead("exported.mjo"));
-			var script3 = Disassembler.DisassembleScript(reader2);
-
-			ControlFlowPass.Analyze(script3);
-
-			using(var fw = new StreamWriter("script3.mjil"))
-				Disassembler.PrintScript(script3, new StreamColorWriter(fw));
-			Disassembler.PrintScript(script3, IColoredWriter.Console);
 		}
 	}
 }
