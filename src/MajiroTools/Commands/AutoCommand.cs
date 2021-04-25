c5543174bba9f1ee3c810ea83fc566ffb23b71fd
AtomCrafty <atomcrafty@frucost.net>
2021-04-25 22:28:46 +0200

    Auto command

---
+++ src/MajiroTools/Commands/AutoCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using VToolBase.Cli;
+
+namespace MajiroTools.Commands {
+	public class AutoCommand : Command {
+		public AutoCommand(CommandParameters parameters) : base(parameters) { }
+
+		public override string Name => "auto";
+
+		public override string[] Description => new[] {
+			"Automatically decide how to process a file"
+		};
+
+		public override (string syntax, string description)[] Usage => new[] {
+			("\abscript.mjo", "Prints the disassembled script to the console")
+		};
+
+		public override (char shorthand, string name, string fallback, string description)[] Flags => new[] {
+			('q', "quiet", "false", "Disable user-friendly output"),
+			('w', "wait", "false", "Whether to wait after completing the command")
+		};
+
+		public override bool Execute() {
+			if(Arguments.Length == 0) {
+				return CommandManager.TryRun("help");
+			}
+
+			string file = Arguments[0];
+			if(file.EndsWith(".mjo", StringComparison.InvariantCultureIgnoreCase)) {
+				Console.WriteLine("Detected as mjo script. Disassembling.");
+				return CommandManager.TryRun("disassemble", "--print", "--file=false", file);
+			}
+
+			return false;
+		}
+	}
+}
