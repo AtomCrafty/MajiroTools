2b59b483304448ecd8457deec47f1151e72f6c16
AtomCrafty <atomcrafty@frucost.net>
2021-05-05 03:55:31 +0200

    Optimize unhash speed

--- src/MajiroTools/Commands/UnhashCommand.cs
+++ src/MajiroTools/Commands/UnhashCommand.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using Majiro;
 using Majiro.Script;
 using VToolBase.Cli;
+using VToolBase.Core;
 
 namespace MajiroTools.Commands {
 	public class UnhashCommand : Command {
@@ -32,7 +34,9 @@ namespace MajiroTools.Commands {
 			('s', "syscall", "false", $"Shortcut for \ac--group={Data.SyscallGroup}"),
 			('l', "local", "false", "Prepend \ab_\a- to the prefix and append \ab@\a- to the suffix"),
 			('g', "group", null, "Prepend \ab$\a- to the prefix and append \ab@<group>\a- to the suffix"),
-
+			
+			('h', "humanize", "false", "Display large numbers with k/m/b/t suffix"),
+			('v', "verbose", "false", "Output extra information"),
 			('q', "quiet", "false", "Disable user-friendly output"),
 			('w', "wait", "false", "Whether to wait after completing the command")
 		};
@@ -42,10 +46,27 @@ namespace MajiroTools.Commands {
 			if(Arguments.Length == 0)
 				throw new ArgumentException("Not enough arguments");
 
-			if(!uint.TryParse(Arguments[0], NumberStyles.HexNumber, CultureInfo.InvariantCulture, out uint targetHash))
-				throw new ArgumentException("Invalid hash. Please specify it as a 32 bit hex number");
+			uint target;
+			HashSet<uint> targets;
+			var hashStrings = Arguments[0].Split(',');
 
-			bool isSyscall = Data.SyscallHashes.Contains(targetHash);
+			if(hashStrings.Length == 1) {
+				targets = null;
+				if(!uint.TryParse(Arguments[0], NumberStyles.HexNumber, CultureInfo.InvariantCulture, out target))
+					throw new ArgumentException($"\acInvalid hash [\ab{Arguments[0]}\ac]. Please specify it as a 32 bit hex number");
+			}
+			else {
+				target = 0;
+				targets = new HashSet<uint>();
+				foreach(string hashString in hashStrings) {
+					if(!uint.TryParse(hashString, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out target))
+						throw new ArgumentException($"\acInvalid hash [\ab{hashString}\ac]. Please specify it as a 32 bit hex number");
+					targets.Add(target);
+				}
+			}
+
+			//targets = new HashSet<uint>(Data.SyscallHashes.Except(Data.KnownSyscallNames.Keys));
+
 			int minLength = Parameters.GetInt("min-length", 'm', 0);
 			int maxLength = Parameters.GetInt("max-length", 'n', 10);
 
@@ -60,7 +81,7 @@ namespace MajiroTools.Commands {
 				charset = "_toiswcbphfmderlnagukvyjqxz"; // ordered by how likely a given character is to be the first in a word (text)
 				charset = "_scpdbramtfiehgluwonvjkqyzx"; // ordered by how likely a given character is to be the first in a word (dictionary)
 				charset = "_etaoinshrdlcumwfgypbvkjxqz"; // ordered by letter frequency (text)
-				charset = "_esuarntolcdugpmhbqfvkwzxjq"; // ordered by letter frequency (dictionary)
+				charset = "_esiarntolcdugpmhbyfvkwzxjq"; // ordered by letter frequency (dictionary)
 			}
 
 			if(Parameters.GetBool("syscall", 's', false))
@@ -84,47 +105,71 @@ namespace MajiroTools.Commands {
 			string prefix = parts[0];
 			string suffix = parts[1];
 
-			Log($"Target:  \ab{targetHash:x8}", true);
+			if(targets != null)
+				Log($"Targets: \ab{string.Join("\a-, \ab", targets.Select(hash => hash.ToString("x8")))}", true);
+			else
+				Log($"Target:  \ab{target:x8}", true);
 			Log($"Length:  \ab{minLength}\a- to \ab{maxLength}", true);
 			Log($"Pattern: \ab{prefix}\ac*\ab{suffix}", true);
 			Log($"Charset: \ab{charset}", true);
 			Log("", true);
 
 			var unhasher = new CrcUnhasher {
-				Target = targetHash,
+				Target = target,
+				TargetSet = targets,
 				Prefix = prefix,
 				Postfix = suffix,
 				Charset = charset,
 			};
 
-			ulong totalChecks = 0;
-			uint totalMatches = 0;
-			uint checksSinceLastUpdate = 0;
+			DoUnhash(unhasher, minLength, maxLength);
 
-			var sw = Stopwatch.StartNew();
-			const int updateInterval = 100;
+			Wait();
+			return true;
+		}
+
+		private void DoUnhash(CrcUnhasher unhasher, int minLength, int maxLength) {
+			bool humanize = Parameters.GetBool("humanize", 'h', false);
+			string Humanize(ulong value) => humanize ? TextHelpers.Humanize(value) : value.ToString();
+
+			ulong totalChecks = 0;
+			ulong lengthChecks = 0;
+			uint updateChecks = 0;
+			uint totalMatches = 0;
+
+			var totalTime = Stopwatch.StartNew();
+			var updateTime = Stopwatch.StartNew();
+			const uint updateInterval = 7193827;
 
 			for(int length = minLength; length <= maxLength; length++) {
 				Log($"Searching for matches of length \ab{length}\a-");
 				unhasher.Length = length;
 
+				ulong permutations = unhasher.Permutations;
 				uint lengthMatches = 0;
 
 				foreach(string match in unhasher) {
-					checksSinceLastUpdate++;
-					if(sw.ElapsedMilliseconds > updateInterval) {
-						totalChecks += checksSinceLastUpdate;
-						Output.WriteColored($"Total checks: \ab{totalChecks}\a- Hashes per ms: \ab{(float)checksSinceLastUpdate / sw.ElapsedMilliseconds}\r");
-						sw.Restart();
-						checksSinceLastUpdate = 0;
+					updateChecks++;
+					if(updateChecks == updateInterval) {
+						lengthChecks += updateChecks;
+						Output.WriteColored($"Checks: \ab{Humanize(lengthChecks)}\a- / \ab{Humanize(permutations)}\a- (\ab{(float)lengthChecks / permutations * 100:F2}\a-%) ");
+						Output.WriteColored($"Hashes per ms: \ab{Humanize(updateChecks / (uint)updateTime.ElapsedMilliseconds)}\a- ");
+						Output.WriteColored($"(avg \ab{Humanize((totalChecks + lengthChecks + updateChecks) / (ulong)totalTime.ElapsedMilliseconds)}\a-)   \r");
+						updateTime.Restart();
+						updateChecks = 0;
 					}
 					if(match == null) continue;
+
 					lengthMatches++;
+					uint hash = unhasher.Result!.Value;
+					bool isSyscall = Data.SyscallHashes.Contains(hash);
 					Output.ClearLine();
-					Output.Write(targetHash.ToString("x8"), isSyscall ? ConsoleColor.Red : ConsoleColor.Blue);
-					Output.WriteLine(' ' + match);
+					Output.Write(hash.ToString("x8"), isSyscall ? ConsoleColor.Red : ConsoleColor.Blue);
+					Output.WriteLineColored($" {unhasher.Prefix}\aa{match}\a-{unhasher.Postfix}");
 				}
 
+				totalChecks += lengthChecks;
+				lengthChecks = 0;
 				Output.ClearLine();
 				if(lengthMatches != 0) {
 					Log($"Found \ab{lengthMatches}\a- match{(lengthMatches != 1 ? "es" : "")} of length \ab{length}\a-");
@@ -132,7 +177,7 @@ namespace MajiroTools.Commands {
 				}
 			}
 
-			return true;
+			Log($"Found \ab{totalMatches}\a- total matches at \ab{totalChecks / (ulong)totalTime.ElapsedMilliseconds}\a- hashes per ms");
 		}
 	}
 }
